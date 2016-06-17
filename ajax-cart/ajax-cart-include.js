3a6e166d26497c8ca06f6fe089d9c4ccd8f40d6c
Brecht Waeghe <brechtwaeghe@gmail.com>
2016-06-17 16:30:59 +0200

    ajax cart action update

--- ajax-cart/ajax-cart-include.js
+++ ajax-cart/ajax-cart-include.js
@@ -1,20 +1,27 @@
 // Start DPD Shipping plugin //
-    
+
 var shippingMethod = 'address',
-    $drawerForm = $('.drawer form[action*="/cart"]');
+    $drawerForm = $('.drawer form[action*="/cart"]'),
+    drawerFormAction = $drawerForm.attr('action'),
+    drawerFormActionSuffix = 'method=address&step=contact_information';
+
+if(drawerFormAction.indexOf('?') > 0) {
+  $drawerForm.attr('action', drawerFormAction + '&' + drawerFormActionSuffix);
+} else {
+  $drawerForm.attr('action', drawerFormAction + '?' + drawerFormActionSuffix);
+}
 
 $('.drawer input[name="sw-ajax-method"]').change(function() {
   shippingMethod = $(this).val();
 });
 
-$drawerForm.attr('action', '/cart?method=address&step=contact_information');
-
 $drawerForm.submit(function(e) {
   if(shippingMethod === 'store' || shippingMethod === 'pickup-parcel') {
     e.preventDefault();
     var url = '/cart#' + shippingMethod;
+
     window.location = url;
-    
+
     if(window.location.href == window.location.origin + url) {
       $('#CartDrawer .js-drawer-close button').trigger('click');
       setTimeout(function() {
